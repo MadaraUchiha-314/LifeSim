008e6f3cc493db01bca2ec69d7c5c59c0be4da91
MadaraUchiha-314 <rohithr31@gmail.com>
2018-10-30 16:07:18 +0530

    Trying to update the pin

--- install_pin.sh
+++ install_pin.sh
@@ -1,4 +1,4 @@
 wget https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.7-97619-g0d0c92f4f-gcc-linux.tar.gz
 tar -xvf pin-3.7-97619-g0d0c92f4f-gcc-linux.tar.gz
 mv pin-3.7-97619-g0d0c92f4f-gcc-linux pin_kit
-rm pin-2.14-71313-gcc.4.4.7-linux.tar.gz
+rm pin-3.7-97619-g0d0c92f4f-gcc-linux.tar.gz
