56601047bbd3e53e7868345314836b1157d6a067
MadaraUchiha-314 <rohithr31@gmail.com>
2018-10-30 16:39:55 +0530

    Trying to get the damn gcc work

---
+++ install_gcc.sh
@@ -0,0 +1,4 @@
+sudo apt-get remove gcc g++
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update && sudo apt-get install gcc-4.7 g++-4.7
